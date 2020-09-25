21e37e1688ee35c8fd057e5e0ced6db0869515af
Joshua Essex <essexjosh@gmail.com>
2020-09-25 13:08:11 -0600

    Tweaks language for one of the error messages

--- src/SupervisionSuccess/constants.js
+++ src/SupervisionSuccess/constants.js
@@ -3,4 +3,4 @@ export const ERROR_RESPONSE_NOT_OK = (response) =>
 export const ERROR_NOT_CSV_FETCHED =
   "Could not load the params file, either because the file was not found or it was not of type 'text/csv'";
 export const ERROR_NO_ROWS =
-  "Parser didn't found any data rows, check the documentation in the readme to see the requirements for the file formatting";
+  "Parser didn't find any data rows, check the documentation in the readme to see the requirements for file formatting";
